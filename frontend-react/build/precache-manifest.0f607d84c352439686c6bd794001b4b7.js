0a73773e0d8b774d9253df78358a14d56acb786c
vimalsuresh93 <vimalsuresh1211@gmail.com>
2020-09-21 01:40:20 +0530

    production

---
+++ frontend-react/build/precache-manifest.0f607d84c352439686c6bd794001b4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efb24838dd33f756763ab40e5c0a1ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7dc911eef8b5ab04ee",
+    "url": "/static/css/main.33d5b387.chunk.css"
+  },
+  {
+    "revision": "d2133c0d06e9e0f7c70c",
+    "url": "/static/js/2.2caf5032.chunk.js"
+  },
+  {
+    "revision": "0718f94d3f72f271a4d3f857026da882",
+    "url": "/static/js/2.2caf5032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7dc911eef8b5ab04ee",
+    "url": "/static/js/main.4e48ce5f.chunk.js"
+  },
+  {
+    "revision": "67a04c5d1af09b613ab7",
+    "url": "/static/js/runtime-main.11c247de.js"
+  }
+]);
